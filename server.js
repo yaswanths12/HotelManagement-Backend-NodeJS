40f0a7a52dedef5469b2e4d7b5e4cb95b77378e1
yaswanths12 <yaswanthprathipati9@gmail.com>
2024-06-13 00:00:14 +0530

    added env

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const db=require('./db')
 
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
 
@@ -10,22 +9,10 @@ app.get('/', function (req, res) {
   res.send('Hello,Welocome to the hotel ,how can I help uh..We have list of items in Menu !')
 })
 
-// app.get('/chicken',function(req,res){
-//     var Cust_Chicken={
-//         name:"fresh chicken",
-//         quantity:2,
-//         is_legpieces:true,
-//     }
-//     res.send(Cust_Chicken)
-// })
-
-
-
-
-
 //Import router files
 const personRoutes = require('./routes/personRoutes')
 const menuItemRoutes = require('./routes/menuItemRoutes')
+
 //Use routers
 app.use('/person',personRoutes)
 app.use('/menu',menuItemRoutes)
